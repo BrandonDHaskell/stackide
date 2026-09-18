5df679547468163e9f629cf15ffb7713d7cffb0a
BrandonDHaskell <BrandonDHaskell@outlook.com>
2026-09-17 23:12:52 -0700

    checking workflow and triggers

--- src/stackide/main.cpp
+++ src/stackide/main.cpp
@@ -2,5 +2,9 @@
 // Created by bhaskell on 9/14/26.
 //
 #include <print>
+#include <version>
 
-int main() { std::println("stackide"); return 0; }
+int main() {
+  std::println("value: {}", 42);
+  //std::println("stackide"); return 0;
+}
